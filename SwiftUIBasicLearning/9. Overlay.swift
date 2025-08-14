737232922528538b20682a842283470b6ff22298
Nick <nick.wysmyk@gmail.com>
2025-08-14 14:20:11 +0200

    testing and playing with tools

--- SwiftUIBasicLearning/9. Overlay.swift
+++ SwiftUIBasicLearning/9. Overlay.swift
@@ -1,5 +1,5 @@
 //
-//  9. Overlay.swift
+//  testing.swift
 //  SwiftUIBasicLearning
 //
 //  Created by Nick Wysmyk on 14.08.25.
@@ -7,36 +7,35 @@
 
 import SwiftUI
 
-struct ___Overlay: View {
+struct Overlay: View {
     var body: some View {
         Image(systemName: "heart.fill")
-            .font(.system(size: 40))
-            .foregroundColor(.white)
+            .foregroundColor(Color.white)
+            .font(.system(size: 100))
             .background(
                 Circle()
                     .fill(
-                        LinearGradient(
-                            gradient: Gradient(colors: [ Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))]),
-                            startPoint: .topLeading,
-                            endPoint: .bottomTrailing)
+                        AngularGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.4492097711, green: 0.2423260637, blue: 0.8632249186, alpha: 1)), Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)),]), center: .topLeading, angle: .degrees(180 + 45))
                     )
-                    .frame(width: 100, height: 100)
-                    .shadow(color: Color("CustomColor"), radius: 10, x: 0.0, y: 10)
+                    .frame(width: 200, height: 200)
+                    .shadow(color: Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)), radius: 30, y: 10)
+            )
+            .frame(width: 200, height: 200)
+            .overlay(
+                Circle()
+                    .fill(Color.blue)
+                    .frame(width: 70, height: 70)
                     .overlay(
-                        Circle()
-                            .fill(Color.blue)
-                            .frame(width: 35, height: 35)
-                            .overlay(
-                                Text("5")
-                                    .font(.headline)
-                                    .foregroundColor(Color.white)
-                            )
-                            .shadow(color: Color("CustomColor"), radius: 10, x: 5, y: 5)
-                        , alignment: .bottomTrailing)
+                            Text("5")
+                                .font(.system(size: 30))
+                                .foregroundColor(Color.white)
+                        , alignment: .center)
+                    .shadow(color: Color.blue, radius: 20, y: 10)
+                , alignment: .bottomTrailing
             )
     }
 }
 
 #Preview {
-    ___Overlay()
+    Overlay()
 }
