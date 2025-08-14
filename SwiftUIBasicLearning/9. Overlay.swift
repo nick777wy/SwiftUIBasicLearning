1196c0bdbd62ca70e7a5a18c0e713c99078ac349
Nick <nick.wysmyk@gmail.com>
2025-08-14 11:52:40 +0200

    Overlay and background finish

--- SwiftUIBasicLearning/9. Overlay.swift
+++ SwiftUIBasicLearning/9. Overlay.swift
@@ -9,18 +9,30 @@ import SwiftUI
 
 struct ___Overlay: View {
     var body: some View {
-        Circle()
-            .fill(Color.pink)
-            .frame(width: 100, height: 100)
-            .overlay(
-                Text("1")
-                    .font(.largeTitle)
-                    .foregroundColor(Color.white)
-            )
+        Image(systemName: "heart.fill")
+            .font(.system(size: 40))
+            .foregroundColor(.white)
             .background(
                 Circle()
-                    .fill(Color.purple)
-                    .frame(width: 120, height: 120)
+                    .fill(
+                        LinearGradient(
+                            gradient: Gradient(colors: [ Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))]),
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing)
+                    )
+                    .frame(width: 100, height: 100)
+                    .shadow(color: Color("CustomColor"), radius: 10, x: 0.0, y: 10)
+                    .overlay(
+                        Circle()
+                            .fill(Color.blue)
+                            .frame(width: 35, height: 35)
+                            .overlay(
+                                Text("5")
+                                    .font(.headline)
+                                    .foregroundColor(Color.white)
+                            )
+                            .shadow(color: Color("CustomColor"), radius: 10, x: 5, y: 5)
+                        , alignment: .bottomTrailing)
             )
     }
 }
